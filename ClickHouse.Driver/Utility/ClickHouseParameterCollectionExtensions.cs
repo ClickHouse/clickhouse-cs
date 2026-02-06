475d5501078cac017a4875181328953404220aa8
Alex Soffronow-Pagonidis <alex.soffronow@clickhouse.com>
2026-02-06 09:32:52 +0100

    Add AddParameter convenience method

---
+++ ClickHouse.Driver/Utility/ClickHouseParameterCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using ClickHouse.Driver.ADO.Parameters;
+
+namespace ClickHouse.Driver.Utility;
+
+public static class ClickHouseParameterCollectionExtensions
+{
+    public static void AddParameter(this ClickHouseParameterCollection parameters, string parameterName, object value)
+    {
+        parameters.Add(new ClickHouseDbParameter { ParameterName = parameterName, Value = value });
+    }
+}
