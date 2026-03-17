6e1c2fac59aae507a5d9e80f517464cee447d2e3
Alex Soffronow-Pagonidis <alex.soffronow@clickhouse.com>
2026-03-17 11:54:36 +0100

    disable until 26.4

--- ClickHouse.Driver.Tests/Types/VariantTests.cs
+++ ClickHouse.Driver.Tests/Types/VariantTests.cs
@@ -82,7 +82,7 @@ public class VariantTests : AbstractConnectionTestFixture
 
     [Test]
     [RequiredFeature(Feature.Variant)]
-    [FromVersion(26, 3)]
+    [FromVersion(26, 4)]
     public async Task ParameterizedSelect_Null_ShouldReturnDbNull()
     {
         using var command = connection.CreateCommand();
